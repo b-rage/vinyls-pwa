e5672583969312f6b5a426b58b943c91361591d6
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2020-05-24 01:33:58 +0200

    add upload image

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { withRouter } from "react-router-dom";
 import { WithStoreConsumer } from "../store";
 import { firebaseApp } from '../../firebase';
--- src/components/pages/Profile/index.js
+++ src/components/pages/Profile/index.js
@@ -1,90 +1,83 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { WithStoreConsumer } from "../../store";
+import { firebaseApp } from '../../../firebase';
+import Resizer from 'react-image-file-resizer';
+import {base64StringToBlob}  from 'blob-util';
 
 
 const Profile = (props) => {
 
+  const [imageUrl, setImageUrl] = useState('');
+
     useEffect(() => {
-        props.context.setPageName('profile')
-        console.log('CONTACTS---------')
-    
+        props.context.setPageName('profile')    
     },[]);
 
-  return (
-    <div className="home">
-      <div className="row">
+  const onUpload = async event => {
 
-      </div>
-      Profile
+    let fileInput = false
+    if(event.target.files && event.target.files[0]) {
+        fileInput = true
+    }
+
+    if(fileInput) {
+
+      const file = event.target.files[0];
+
+      let _type;
+
+      if(file.type === 'image/jpeg') {
+        _type = 'JPEG';
+      }else if(file.type === 'image/png') {
+        _type = 'PNG';
+      }
       
-      home
+      const resizeFile = (file) => new Promise(resolve => {
+        Resizer.imageFileResizer(file, 500, 500, _type, 100, 0,
+        uri => {
+          resolve(uri);
+        },
+        'base64'
+        );
+      });
+
+      const _image = await resizeFile(file);
+      const contentType = file.type;
+      let b64Data;
+      if(file.type === 'image/jpeg') {
+        b64Data = _image.slice(23);
+      }else if(file.type === 'image/png') {
+        b64Data = _image.slice(22);
+      }
+      const blob = base64StringToBlob(b64Data, contentType);
+
+      setTimeout(function(){ 
+        const storageRef = firebaseApp.storage().ref(`/userImg/${file.name}`);
+        const task = storageRef.put(blob);
+
+        task.then(snapshot => {
+            return snapshot.ref.getDownloadURL()
+          }).then(downUrl => {
+              setImageUrl(downUrl);
+          }); 
+      }, 100);
+
+    
+    }
+  }
+
+  return (
+      <>
       <br></br>
-      home
       <br></br>
-      home
       <br></br>
-      home
       <br></br>
-      home
       <br></br>
-      home
       <br></br>
-      home<br></br>
-      <br></br>home home
       <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home<br></br>
-      <br></br>home home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home<br></br>
-      <br></br>home home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home<br></br>
-      <br></br>home home<br></br>
-      <br></br>home home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home
-      <br></br>
-      home<br></br>
-      <br></br>home
-    </div>
+          <input className="input-class" type="file" onChange={onUpload}/>
+          <img className="image-class"  src={imageUrl} alt='' />
+      </>
   );
 };
 
